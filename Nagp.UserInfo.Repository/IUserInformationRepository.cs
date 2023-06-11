305d66e03bc8456dd341c9271f1797f0ea21cc4f
akshay <akshay.kaundal@nagarro.com>
2023-06-11 14:03:54 +0530

    checkign in code removed

--- Nagp.UserInfo.Repository/IUserInformationRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nagp.UserInfo.Repository.Models;
-
-namespace Nagp.UserInfo.Repository
-{
-    public interface IUserInformationRepository
-    {
-        IEnumerable<UserInformation> GetAllUserInformationRepository();
-    }
-}
