0495f379092d47893dc9802fe0f597639c6f072e
akshay <akshay.kaundal@nagarro.com>
2023-06-15 23:33:04 +0530

    checking in config file change

--- Nagp.UserInfo/Models/NagpUserDbContext.cs
+++ Nagp.UserInfo/Models/NagpUserDbContext.cs
@@ -17,9 +17,9 @@ public partial class NagpUserDbContext : DbContext
 
     public virtual DbSet<UserInformation> UserInformations { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Initial Catalog=NagpUserDb;Persist Security Info=True;TrustServerCertificate=True;User Id=SA;Password=1Secure*Password1; connect timeout=500");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=.;Initial Catalog=NagpUserDb;Persist Security Info=True;TrustServerCertificate=True;User Id=SA;Password=1Secure*Password1; connect timeout=500");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
