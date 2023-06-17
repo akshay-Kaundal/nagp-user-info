e8340a9b035aa5efead4899baf822a4f27ba713a
akshay <akshay.kaundal@nagarro.com>
2023-06-17 08:52:25 +0530

    checking in git config changes

--- Nagp.UserInfo/Program.cs
+++ Nagp.UserInfo/Program.cs
@@ -6,9 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var db_Password_Env = Environment.GetEnvironmentVariable("DB_PASSWORD_ENV");
 var db_Server_Name_Env = Environment.GetEnvironmentVariable("SERVER_NAME_ENV");
-var db_Port_No_Env = Environment.GetEnvironmentVariable("PORT_NO_ENV");
+//var db_Port_No_Env = Environment.GetEnvironmentVariable("PORT_NO_ENV");
 var connectionString = builder.Configuration.GetConnectionString("NagpUserDB");
-var finalConnString = string.Format(connectionString, db_Server_Name_Env, db_Port_No_Env, db_Password_Env);
+var finalConnString = string.Format(connectionString, db_Server_Name_Env, db_Password_Env);
 builder.Services.AddDbContext<NagpUserDbContext>(option =>
 option.UseSqlServer(finalConnString)
 );
